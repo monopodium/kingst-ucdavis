67c4912bd39b2d855ba04aeec939590c23d41df7
Sam King <kingst@gmail.com>
2026-05-01 10:49:47 -0700

    Bump reading list

---
+++ inclass_programming/s26-ecs150/threads/consumer.cpp
@@ -0,0 +1,17 @@
+int numCokes = 0;
+int maxCokes = 100;
+mutex_t cokeLock;
+// wait until hasRoom
+cond_t hasRoom;
+// wait until hasCoke
+cond_t hasCoke;
+CokeMachine machine;
+
+void consumer() {
+  cokeLock.lock();
+  
+  machine.removeCoke();
+  numCokes--;
+  
+  cokeLock.unlock();
+}
