51beac7c4b58cbbe96fe7952233a28b0e57e8a55
Sam King <kingst@gmail.com>
2026-05-06 10:59:13 -0700

    Bump reading list

--- inclass_programming/s26-ecs150/threads/rwlock.cpp
+++ inclass_programming/s26-ecs150/threads/rwlock.cpp
@@ -4,30 +4,13 @@ mutex_t lock;
 cond_t unlockCalled;
 
 void reader() {
-  lock.lock();
-  while (writerActive) {
-    wait(unlockCalled, lock);
-  }
-  numReaders += 1;
-  lock.unlock();
+
 }
 
 void writer() {
-  lock.lock();
-  while (numReaders > 0) {
-    wait(unlockCalled, lock);
-  }
-  writerActive = true;
-  lock.unlock();
+
 }
 
-void rwunlock() {
-  lock.lock();
-  if (numReaders > 0) {
-    numReader -= 1;
-  } else {
-    writerActive = false;
-  }
-  broadcast(unlockCalled);
-  lock.unlock();
+void unlock() {
+
 }
